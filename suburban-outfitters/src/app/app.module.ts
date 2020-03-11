99e2fde906894b25d5efcbfe6c79f601622c2e7d
jspenc72 <jspenc72@gmail.com>
2020-03-10 20:39:09 -0600

    generate components
    - admin-dashboard
    - customer-dashboard
    - password-reset
    - product-detail
    - products

--- suburban-outfitters/src/app/app.module.ts
+++ suburban-outfitters/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    CustomerDashboardComponent,
+    AdminDashboardComponent,
+    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
