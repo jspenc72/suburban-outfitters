8166c6fefdcb19c2bc394b6f88d9967ca318901d
jspenc72 <jspenc72@gmail.com>
2020-03-11 20:28:04 -0600

    Update products page.

--- suburban-outfitters/src/app/products/products.component.ts
+++ suburban-outfitters/src/app/products/products.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  items: any
+  featuredItems: any
+  constructor() { 
+    this.featuredItems = [
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=1", description: "Product 1", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10}]
+    this.items = [
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=1", description: "Product 1", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10},
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=4", description: "Product 4", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=5", description: "Product 5", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=6", description: "Product 6", points: 10},
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=7", description: "Product 7", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=8", description: "Product 8", points: 10}, 
+      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=9", description: "Product 9", points: 10}]
+  }
 
   ngOnInit(): void {
   }
