e8fd559e3a0794b1dd30a7709c29ea89857b0bd4
jspenc72 <jspenc72@gmail.com>
2020-03-11 20:03:54 -0600

    Setup basic Routing
    Remove boilerplate from app.component.

--- suburban-outfitters/src/app/app-routing.module.ts
+++ suburban-outfitters/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', component: ProductsComponent },
+  { path: '**', component: HomeComponent } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
