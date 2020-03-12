e2c3f13fa2de5d087fe841a6c71fb8b64d104b82
jspenc72 <jspenc72@gmail.com>
2020-03-12 00:03:50 -0600

    Update cart component.

--- suburban-outfitters/src/app/cart/cart.component.ts
+++ suburban-outfitters/src/app/cart/cart.component.ts
@@ -10,8 +10,15 @@ export class CartComponent implements OnInit {
   isLinear = false;
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-
-  constructor(private _formBuilder: FormBuilder) {}
+  cartItems: any;
+  panelOpenState = false;
+  constructor(private _formBuilder: FormBuilder) {
+    this.cartItems = [
+      { value: 100, image: "https://picsum.photos/200?random=1", description: "Product 1", points: 10}, 
+      { value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
+      { value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10}]
+    
+  }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
