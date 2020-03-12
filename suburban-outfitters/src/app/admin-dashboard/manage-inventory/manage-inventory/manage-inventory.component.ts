f7c7495f8ecc7f1f27ff23eab9efdf4d4262b0b8
jspenc72 <jspenc72@gmail.com>
2020-03-12 16:42:16 -0600

    Added tabs for admin-dashboard.

--- suburban-outfitters/src/app/admin-dashboard/manage-inventory/manage-inventory/manage-inventory.component.ts
+++ suburban-outfitters/src/app/admin-dashboard/manage-inventory/manage-inventory/manage-inventory.component.ts
@@ -1,4 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  phone: string;
+  abbreviation: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 2, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 3, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 4, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 5, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 6, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 7, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 8, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 9, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+  {position: 10, name: 'Customer Name', phone: "1-801-456-789", abbreviation: 'C1'},
+];
 
 @Component({
   selector: 'app-manage-inventory',
@@ -6,7 +25,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-inventory.component.scss']
 })
 export class ManageInventoryComponent implements OnInit {
-
+  @Input() inventory: any;
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
