61471467071002986c551770dec23e4ac12711d9
jspenc72 <jspenc72@gmail.com>
2020-03-11 21:16:31 -0600

    Update product search page

--- suburban-outfitters/src/app/products/products.component.ts
+++ suburban-outfitters/src/app/products/products.component.ts
@@ -11,18 +11,19 @@ export class ProductsComponent implements OnInit {
   constructor() { 
     this.featuredItems = [
       { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=1", description: "Product 1", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10}]
+      { title: "card2", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
+      { title: "card3", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10}]
     this.items = [
       { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=1", description: "Product 1", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10},
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=4", description: "Product 4", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=5", description: "Product 5", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=6", description: "Product 6", points: 10},
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=7", description: "Product 7", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=8", description: "Product 8", points: 10}, 
-      { title: "card1", type: "", value: 100, image: "https://picsum.photos/200?random=9", description: "Product 9", points: 10}]
+      { title: "card2", type: "", value: 100, image: "https://picsum.photos/200?random=2", description: "Product 2", points: 10}, 
+      { title: "card3", type: "", value: 100, image: "https://picsum.photos/200?random=3", description: "Product 3", points: 10},
+      { title: "card4", type: "", value: 100, image: "https://picsum.photos/200?random=4", description: "Product 4", points: 10}, 
+      { title: "card5", type: "", value: 100, image: "https://picsum.photos/200?random=5", description: "Product 5", points: 10}, 
+      { title: "card6", type: "", value: 100, image: "https://picsum.photos/200?random=6", description: "Product 6", points: 10},
+      { title: "card7", type: "", value: 100, image: "https://picsum.photos/200?random=7", description: "Product 7", points: 10}, 
+      { title: "card8", type: "", value: 100, image: "https://picsum.photos/200?random=8", description: "Product 8", points: 10}, 
+      { title: "card9", type: "", value: 100, image: "https://picsum.photos/200?random=9", description: "Product 9", points: 10},
+      { title: "card10", type: "", value: 100, image: "https://picsum.photos/200?random=10", description: "Product 10", points: 10}]
   }
 
   ngOnInit(): void {
