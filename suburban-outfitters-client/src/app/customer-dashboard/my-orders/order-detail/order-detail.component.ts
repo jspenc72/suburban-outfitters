09017ad9cc81dd5878acacfd5004d4593a4bb1c6
Brandon Mortensen <btmorty@gmail.com>
2020-04-19 16:42:07 -0600

    add order detail model

--- suburban-outfitters-client/src/app/customer-dashboard/my-orders/order-detail/order-detail.component.ts
+++ suburban-outfitters-client/src/app/customer-dashboard/my-orders/order-detail/order-detail.component.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes, Router, RouterState, ActivatedRoute, Params } fro
 import { AuthService } from '../../../services/auth.service';
 import { OrdersService } from '../../../services/orders.service';
 import { OrderLineItemService } from '../../../services/order-line-item.service';
-import { IOrderResponse } from 'src/app/models/order.model';
+import { IOrderResponse, IOrderDetial } from 'src/app/models/order.model';
 import { IOrderLineItem } from 'src/app/models/order-line-item';
 
 @Component({
@@ -13,25 +13,18 @@ import { IOrderLineItem } from 'src/app/models/order-line-item';
 })
 export class OrderDetailComponent implements OnInit {
   order_id: number;
-  order: IOrderResponse;
-  orderLineItems: IOrderLineItem[];
+  order: IOrderDetial;
   constructor(private router: Router,
     private route: ActivatedRoute,
     private authService: AuthService,
     private ordersService: OrdersService,
     private orderlineItemService: OrderLineItemService
-  ) {
-    // cookieService.set('laravel_session', 'value')
-    // console.log('laravel_session', cookieService.getAll())
-
-  }
+  ) { }
 
   loadOrderDetails(id: any) {
     this.ordersService.getBy(id).subscribe((data: any) => {
-      console.log(data)
       this.order = data;
-
-    })
+    });
   }
 
 
--- suburban-outfitters-client/src/app/models/order.model.ts
+++ suburban-outfitters-client/src/app/models/order.model.ts
@@ -1,3 +1,5 @@
+import { IOrderLineItem } from './order-line-item';
+
 export interface IOrderRequest {
   customer_id: number;
   order_status_id: number;
@@ -19,4 +21,17 @@ export interface IOrderResponse {
   delivery_date?: Date;
   purchase_date?: Date;
   return_date?: Date;
+}
+
+export interface IOrderDetial {
+  id: number;
+  customer_id: number;
+  order_status_id: number;
+  order_total: number;
+  order_date: Date;
+  departure_date?: Date;
+  delivery_date?: Date;
+  purchase_date?: Date;
+  return_date?: Date;
+  orderlineitems: IOrderLineItem[];
 }
